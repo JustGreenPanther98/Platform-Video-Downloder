defc6e648950fc9ca6cdfabbece7f0f466fe9eef
Vedant Maheshwari <vedantmaheshwari2006@gmail.com>
2026-02-08 01:25:10 +0530

    swagger configuration updation

--- src/main/java/com/video/configurations/SwaggerConfiguration.java
+++ src/main/java/com/video/configurations/SwaggerConfiguration.java
@@ -1,20 +1,37 @@
 package com.video.configurations;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfiguration {
 	@Bean
 	OpenAPI myConfiguration() {
 		OpenAPI openAPI = new OpenAPI();
-		
+
 		Info info = new Info();
-		info.setContact(new Contact());
+		info.setTitle("Video Downloder API");
+		info.setDescription("Download videos you want from top known platform!");
+		info.setSummary(
+				"API uses some external API like Rapid API\n through which you can download long videos or shorts ");
+		Contact contact = new Contact();
+		contact.setName("Vedant Maheshwari");
+		contact.setUrl("https://github.com/JustGreenPanther98");
+		contact.setEmail("zippyjese@gmail.com");
+		info.setContact(contact);
+
+		openAPI.setInfo(info);
+
+		openAPI.setServers(List.of(new Server().url("http://localhost:2026").description("LOCAL"),
+				new Server().url("").description("PUBLIC")));
+
 		return openAPI;
 	}
 
